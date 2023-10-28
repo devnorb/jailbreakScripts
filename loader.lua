b428dcd77f0643088ba5c3699fded74ddf980929
norb_ <127006485+devnorb@users.noreply.github.com>
2023-10-28 11:13:32 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,15 @@
+--[[
+     ____.      .__.__ ___.                        __      ____ ___      .__                                 .__   
+    |    |____  |__|  |\_ |_________  ____ _____  |  | __ |    |   \____ |__|__  __ ___________  __________  |  |  
+    |    \__  \ |  |  | | __ \_  __ \/ __ \\__  \ |  |/ / |    |   /    \|  \  \/ // __ \_  __ \/  ___|__  \ |  |  
+/\__|    |/ __ \|  |  |_| \_\ \  | \|  ___/ / __ \|    <  |    |  /   |  \  |\   /\  ___/|  | \/\___ \ / __ \|  |__
+\________(____  /__|____/___  /__|   \___  >____  /__|_ \ |______/|___|  /__| \_/  \___  >__|  /____  >____  /____/
+              \/            \/           \/     \/     \/              \/              \/           \/     \/      
+
+Jailbreak Universal loader script by norb_
+Deobfuscation attempts will result in a HWID script ban 
+]]--
+
+-- print(game.Players.LocalPlayer.Character.HumanoidRootPart.Position)
+
+loadstring(game:HttpGet("https://cdn.discordapp.com/attachments/1167250916123082752/1167888669605118054/legacy.lua"))()
